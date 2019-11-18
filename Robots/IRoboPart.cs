cabe20fd988bb286f1a6efb91fa1277e95b1fcf7
OndraBednar <ondrej.bednar@pslib.cz>
2019-11-18 21:50:19 +0100

    roboti

---
+++ Robots/IRoboPart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robots
+{
+    interface IRoboPart
+    {
+        virtual string Message { get; }
+        double Consumption { get; }
+        virtual double DeviceConsumption { get; protected set;}
+    }
+}
